9de1689c7abdc8efc1bdd4b105dc65bb6ca43b9b
Tim Smith <tsmith84@gmail.com>
2022-05-04 23:36:48 -0700

    Handle the secret
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-mondoo scan -t $INPUT_SCAN_TYPE --path $INPUT_PATH --incognito
+if [ -z "$MONDOO_AGENT_ACCOUNT" ]; then
+  echo "The Mondoo service account token was not set in the GitHub Actions config. Cannot continue."
+  exit 1
+fi
+
+mondoo scan -t $INPUT_SCAN_TYPE --path $INPUT_PATH
